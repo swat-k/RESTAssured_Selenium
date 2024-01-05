5476dc4be7c0598c5ce8af5abaf6f2f48aa2d9f0
Swathy K <97823860+swat-k@users.noreply.github.com>
2024-01-05 18:02:23 +0530

    JSONschema Validation for other endpoints

--- src/test/java/TestExamples/JsonValidator.java
+++ src/test/java/TestExamples/JsonValidator.java
@@ -8,11 +8,41 @@ import org.testng.annotations.Test;
 public class JsonValidator {
 
 	@Test
-	public void test2()
+	public void test1()
 	{
+		// validating schema for 2 page users
 		
 		baseURI="https://reqres.in/api";
 		given().
-		get("/users?page=2").then().assertThat().body(matchesJsonSchemaInClasspath("schema.json")).statusCode(200);
+		get("/users?page=2").then().assertThat().body(matchesJsonSchemaInClasspath("usersschema.json")).statusCode(200);
 }
+	@Test
+	public void transactiontest()
+	{
+		
+		given().queryParam("CUSTOMER_ID", "68195")
+	       .queryParam("PASSWORD", "1234!")
+	       .queryParam("Account_No", "1")
+	.get("http://demo.guru99.com/V4/sinkministatement.php").then()
+		  .assertThat()
+		  .body(matchesJsonSchemaInClasspath("transactionschema.json"));
+	}
+	
+	@Test
+	public void singleuser()
+	
+	{
+		baseURI="https://reqres.in/api";
+		given().get("/users/2").then().assertThat().body(matchesJsonSchemaInClasspath("singleuserschema.json"));
+	}
+	
+	@Test
+	public void postuser()
+	
+	{
+		baseURI="https://reqres.in/api";
+		given().post("/users").then().assertThat().body(matchesJsonSchemaInClasspath("postuser.json"));
+	}
+	
+	
 }
